905ece1ec9b59f4ecc05284d7c32ef1e4f3b4c50
Patricio Costilla <patriciocostilla@gmail.com>
2023-06-20 13:10:32 -0300

    c2. deactivate external scoring check

--- challenge-2/backend/apps/request_loan/forms.py
+++ challenge-2/backend/apps/request_loan/forms.py
@@ -10,15 +10,15 @@ class RequestLoanModelForm(forms.ModelForm):
         model = RequestLoan
         fields = ['gender', 'email', 'document_number', 'full_name', 'amount']
 
-    def clean(self):
-        cleaned_data = super().clean()
-        url = "http://endpoint.test.com.ar:7001/api/v1/scoring/?document_number={0}&gender={1}&email={2}"\
-            .format(cleaned_data['document_number'], cleaned_data['gender'].upper(), cleaned_data['email'])
-        response = requests.get(url)
-        if response.json().get('error'):
-            raise forms.ValidationError("Hubo un error en el servidor, vuelva a intentarlo mas tarde.")
+    # def clean(self):
+    #     cleaned_data = super().clean()
+    #     url = "http://endpoint.test.com.ar:7001/api/v1/scoring/?document_number={0}&gender={1}&email={2}"\
+    #         .format(cleaned_data['document_number'], cleaned_data['gender'].upper(), cleaned_data['email'])
+    #     response = requests.get(url)
+    #     if response.json().get('error'):
+    #         raise forms.ValidationError("Hubo un error en el servidor, vuelva a intentarlo mas tarde.")
 
-        return cleaned_data
+    #     return cleaned_data
 
     def clean_amount(self):
         data = self.cleaned_data['amount']
